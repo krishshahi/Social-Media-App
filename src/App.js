d26d95bb9691d1b2230ccec0f29899e5e2db5bd6
KXS Plays <shahikrish666@gmail.com>
2022-07-13 23:06:38 +0545

    Interface

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 import { useDispatch } from "react-redux";
@@ -10,6 +10,7 @@ import Form from './components/Form/Form'
 import memories from './images/memories.png';
 import useStyles from './styles';
 const App = () => {
+    const [currentId, setCurrentId] = useState(null);
     const classes = useStyles();
     const dispatch = useDispatch();
 
@@ -20,17 +21,17 @@ const App = () => {
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <Typography className={classes.heading} variant="h2" align="center">Souvenirs</Typography>
                 <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts />
+                            <Posts setCurrentId={setCurrentId} />
                         </Grid>
                         <Grid item xs={12} sm={4}>
-                            <Form />
+                            <Form currentId={currentId} setCurrentId={setCurrentId} />
                         </Grid>
                     </Grid>
                 </Container>
